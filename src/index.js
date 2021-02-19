8ed0d23719e42d375c09a79965693c9d8530ebee
Kirill Romanovskii <whitekvazar@gmail.com>
2021-02-19 17:40:34 +0300

    feat: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let tempFlag = true;
+    while(tempFlag) {
+      tempFlag = false;
+      for (let i = 0; i < bracketsConfig.length; i++) {
+        let spliter = bracketsConfig[i][0] + bracketsConfig[i][1]; // '()' for example
+        let splitedStrArray = str.split(spliter);
+        
+        
+        if(splitedStrArray.length > 1) {  // splitedStrArray[] always will have atleast one element
+          tempFlag = true;                // even if we .split last pair of brackets - it'll be [''] element
+          str = splitedStrArray.join(''); // and str here will be '' if splitedStrArray[] = ['']
+        }
+      } 
+    }
+                                          
+  return (str) ? false : true;            // if all brackets will have a pair str will be '' == false
 }
